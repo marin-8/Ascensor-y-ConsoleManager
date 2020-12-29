cb080be56304cbd8540b43c6b14fc24daf9010f0
mrsamisack <marin.tecnico.8@gmail.com>
2020-12-29 17:40:06 +0100

    Probando cosillas y guardando por si acaso

--- Manual Demo/consoleManager.py
+++ Manual Demo/consoleManager.py
@@ -1,11 +1,45 @@
 
+# ===== IMPORTS ==================================================================================================== #
+
+from os import system
+
 # ===== CONSOLE MANAGER ==================================================================================================== #
 
 class CM:
 
+    __commands = ("play", "pause",
+                  "setplaybackspeed",
+                  "nextstep", "nexttarget",
+                  "addtarget",
+                  "clear", "exit")
+
+    __USER = "    [USER] > "
+    __ALERT = "   [ALERT] > "
+    __ERROR = "   [ERROR] > "
+    __MESSAGE = " [MESSAGE] > "
+    __INFO = "    [INFO] > "
+
+    play = False
+    playbackSpeed = 2
+    running = True
+
     @staticmethod
     def consoleManager():
-        while True:
-            print(input("Escribe algo: "))
+        while CM.running:
+            command = input(CM.__USER).lower().split()
+
+            if command[0] not in CM.__commands:
+                print(CM.__ALERT + "The command entered does not exist. Try again.")
+            else:
+                if command[0] == "play":
+                    CM.play = True
+                elif command[0] == "pause":
+                    CM.play = False
+                elif command[0] == "setPlaybackSpeed".lower():
+                    CM.playbackSpeed = int(command[1])
+                elif command[0] == "clear":
+                    system('cls')
+                elif command[0] == "exit":
+                    CM.running = False
 
 # ===== ========== ==================================================================================================== #
--- Manual Demo/main.py
+++ Manual Demo/main.py
@@ -4,6 +4,7 @@
 import os
 import pygame
 import threading
+from os import system
 
 from globalStuff import GB
 from consoleManager import CM
@@ -13,60 +14,61 @@ from elevator import Elevator
 from floors import Floors
 from Ascensor.BackEnd.elevator_BackEnd import Elevator_BackEnd
 
+# ===== MAIN ==================================================================================================== #
+
+if __name__ == "__main__":
+
 # ===== SCREEN POSITION ==================================================================================================== #
 
-screen_position_x = GB.SCREEN_WIDTH() - GB.WINDOW_WIDTH()
-screen_position_y = 0
+    screen_position_x = GB.SCREEN_WIDTH() - GB.WINDOW_WIDTH()
+    screen_position_y = 0
 
-os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (screen_position_x, screen_position_y)
+    os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (screen_position_x, screen_position_y)
 
 # ===== PYGAME INIT ==================================================================================================== #
 
-pygame.init()
+    pygame.init()
 
-SCREEN = pygame.display.set_mode((GB.WINDOW_WIDTH(), GB.WINDOW_HEIGHT()), pygame.NOFRAME)
+    SCREEN = pygame.display.set_mode((GB.WINDOW_WIDTH(), GB.WINDOW_HEIGHT()), pygame.NOFRAME)
 
-clock = pygame.time.Clock()
-font = pygame.font.SysFont("Consolas", 24)
+    clock = pygame.time.Clock()
+    font = pygame.font.SysFont("Consolas", 24)
+
+    system('cls')
 
 # ===== ELEVATOR INIT ==================================================================================================== #
 
-elevator = Elevator_BackEnd(GB.NUM_FLOORS())
+    elevator = Elevator_BackEnd(GB.NUM_FLOORS())
 
-elevator.add_target(7, Elevator_BackEnd.FLOOR)
-elevator.add_target(5, Elevator_BackEnd.FLOOR)
-elevator.add_target(5, Elevator_BackEnd.ELEVATOR)
-elevator.add_target(1, Elevator_BackEnd.ELEVATOR)
-
-waitFlag = False
+    elevator.add_target(7, Elevator_BackEnd.FLOOR)
+    elevator.add_target(5, Elevator_BackEnd.FLOOR)
+    elevator.add_target(5, Elevator_BackEnd.ELEVATOR)
+    elevator.add_target(1, Elevator_BackEnd.ELEVATOR)
 
 # ===== LOOP ==================================================================================================== #
 
-threading.Thread(target=CM.consoleManager).start()
+    threading.Thread(target=CM.consoleManager, daemon=True).start()
 
-RUNNING = True
-while RUNNING:
+    while CM.running:
 
-    SCREEN.fill((192, 192, 192))
-    pygame.draw.rect(SCREEN, (64, 64, 64), (0, 0, GB.WINDOW_WIDTH(), GB.FPS_BAR_HEIGHT()))
-    SCREEN.blit(font.render("FPS: " + str(int(clock.get_fps())), True, (192, 192, 192)), (8, 8))
+        SCREEN.fill((192, 192, 192))
+        pygame.draw.rect(SCREEN, (64, 64, 64), (0, 0, GB.WINDOW_WIDTH(), GB.FPS_BAR_HEIGHT()))
+        SCREEN.blit(font.render("FPS: " + str(int(clock.get_fps())), True, (192, 192, 192)), (8, 8))
 
-    DT = clock.tick(2)
+        DT = clock.tick(CM.playbackSpeed)
 
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                RUNNING = False
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    CM.running = False
 
-    if not waitFlag:
-        waitFlag = True
-    else:
-        elevator.next_step()
+        if CM.play:
+            elevator.next_step()
 
-    #Tests.tests(pygame, SCREEN, DT)
-    Elevator.Draw(pygame, SCREEN, elevator.cur_floor, elevator.doorsOpen)
-    Floors.Draw(pygame, SCREEN, elevator.floor_buttons, elevator.elevator_buttons)
+        #Tests.tests(pygame, SCREEN, DT)
+        Elevator.Draw(pygame, SCREEN, elevator.cur_floor, elevator.doorsOpen)
+        Floors.Draw(pygame, SCREEN, elevator.floor_buttons, elevator.elevator_buttons)
 
-    pygame.display.update()
+        pygame.display.update()
 
 # ===== ========== ==================================================================================================== #

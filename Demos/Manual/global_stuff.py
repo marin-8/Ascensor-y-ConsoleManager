5ca6f1319cd395d72f4f400e2f9ee37e865c3147
mrsamisack <marin.tecnico.8@gmail.com>
2021-01-04 16:59:30 +0100

    Renombrados/as las carpetas, archivos y classes de todo el proyecto. Reorganizada la estructura de carpetas y archivos.

---
+++ Demos/Manual/global_stuff.py
@@ -0,0 +1,69 @@
+
+# ===== IMPORTS ==================================================================================================== #
+
+from os import system
+from win32api import GetSystemMetrics
+from Console_Manager.console_manager import ConsoleManager
+
+# ===== GLOBAL STUFF ==================================================================================================== #
+
+class GlobalStuff:
+
+    __SCREEN_WIDTH = GetSystemMetrics(0)
+    __SCREEN_HEIGHT = GetSystemMetrics(1)
+
+    __WINDOW_WIDTH = 180
+    __WINDOW_HEIGHT = __SCREEN_HEIGHT - 40
+
+    __FPS_BAR_HEIGHT = 30
+
+    __MAX_FLOORS = int(-__FPS_BAR_HEIGHT/65+__SCREEN_HEIGHT/65-4/13)
+    __NUM_FLOORS = -1
+
+    # ================================================== #
+
+    @staticmethod
+    def SET_NUM_FLOORS():
+        while True:
+            try:
+                inputFloors = int(ConsoleManager.INPUT_USER
+                (
+                    "Input the number of floors for the elevator (between 2 and " +
+                    str(GlobalStuff.__MAX_FLOORS) +
+                    " (included) for your vertical resolution): ")
+                )
+                assert(2 <= inputFloors <= GlobalStuff.__MAX_FLOORS)
+                GlobalStuff.__NUM_FLOORS = inputFloors
+                break
+            except:
+                system('cls')
+                print()
+                ConsoleManager.LOG_ERROR("You must enter an integer between 2 and " + str(GlobalStuff.__MAX_FLOORS) + " (included).")
+
+    # ================================================== #
+
+    @staticmethod
+    def SCREEN_WIDTH():
+        return GlobalStuff.__SCREEN_WIDTH
+
+    @staticmethod
+    def SCREEN_HEIGHT():
+        return GlobalStuff.__SCREEN_HEIGHT
+
+    @staticmethod
+    def WINDOW_WIDTH():
+        return GlobalStuff.__WINDOW_WIDTH
+
+    @staticmethod
+    def WINDOW_HEIGHT():
+        return GlobalStuff.__WINDOW_HEIGHT
+
+    @staticmethod
+    def FPS_BAR_HEIGHT():
+        return GlobalStuff.__FPS_BAR_HEIGHT
+
+    @staticmethod
+    def NUM_FLOORS():
+        return GlobalStuff.__NUM_FLOORS
+
+# ===== ========== ==================================================================================================== #
